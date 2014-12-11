755a76f760b6fa9abd1e427519eba6b6c0b17521
mejiro <m.sh.nwpct1@gmail.com>
2014-12-11 17:07:58 +0900

    Use a django template and write a test

---
+++ cms/tests/test_return_correct_html.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
+from cms.views import book_list
+
+
+class HtmlTests(TestCase):
+    def test_book_list_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = book_list(request)
+        expected_html = render_to_string('cms/book_list.html',
+                                         {'books': []})
+        self.assertEqual(response.content.decode(), expected_html)
--- cms/views.py
+++ cms/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from cms.models import Book
 
 
 def book_list(request):
     """書籍の一覧"""
-    return HttpResponse(u'書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render_to_response('cms/book_list.html',  # 使用するテンプレート
+                              {'books': books},       # テンプレートに渡すデータ
+                              context_instance=RequestContext(request))  # その他標準のコンテキスト
 
 
 def book_edit(request, book_id=None):
